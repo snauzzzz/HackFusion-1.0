47ce3309157a7322b565eccc9dc0010d50918dcf
NguyenHaThaiSon <mathsislife314@gmail.com>
2024-06-12 12:40:29 +0700

    Added falling animation to interactive elements

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('click', function(event) {
         const randomImage = images[randomIndex]; 
         const leaf = document.createElement('img');
         leaf.src = 'leafs/' + randomImage; 
-        leaf.className = 'leaf';
+        leaf.className = 'leaf falling'; // Added 'falling' class
         // Adjust the position to account for the scroll position
         leaf.style.top = `${event.clientY + window.scrollY - 25}px`;
         leaf.style.left = `${event.clientX + window.scrollX - 25}px`;
@@ -15,10 +15,10 @@ document.addEventListener('click', function(event) {
         // Add fade-out effect after a delay
         setTimeout(() => {
             leaf.style.opacity = '0';
+            leaf.style.transition = 'opacity 3s ease-out'; 
             setTimeout(() => {
-                // Remove the leaf from the DOM after the animation completes
                 leaf.remove(); 
-            }, 1000);
-        }, 2000); 
+            }, 3000); 
+        }, 0); 
     }
 });
