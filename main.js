8068e69ee5c04698c352feb8745523cb8b30c8d5
NguyenHaThaiSon <mathsislife314@gmail.com>
2024-06-12 11:38:30 +0700

    Added random pop-up elements

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 document.addEventListener('click', function(event) {
     // Check if the click is on the body or main content area
-    if (event.target === document.body || event.target.closest('main')) {
+    if (event.target === event.currentTarget || event.target.closest('main')) {
+        const images = ['leaf1.svg', 'leaf2.svg', 'leaf3.svg', 'leaf4.svg', 'leaf5.svg']; 
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const randomImage = images[randomIndex]; 
         const leaf = document.createElement('img');
-        leaf.src = 'photos/leaf.svg';
+        leaf.src = 'leafs/' + randomImage; 
         leaf.className = 'leaf';
         // Adjust the position to account for the scroll position
-        leaf.style.top = `${event.clientY + window.scrollY - 25}px`;
-        leaf.style.left = `${event.clientX + window.scrollX - 25}px`;
+        leaf.style.top = `${event.clientY - 25}px`;
+        leaf.style.left = `${event.clientX - 25}px`;
         document.body.appendChild(leaf);
-        
+
         // Add fade-out effect after a delay
         setTimeout(() => {
-            leaf.classList.add('fade-out');
+            leaf.style.opacity = '0';
+            setTimeout(() => {
+                // Remove the leaf from the DOM after the animation completes
+                leaf.remove(); 
+            }, 1000);
         }, 2000); 
-
-        // Remove the leaf from the DOM after the animation completes
-        leaf.addEventListener('animationend', () => {
-            leaf.remove();
-        });
     }
 });
