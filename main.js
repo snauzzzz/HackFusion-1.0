97b22ff341b6912293e526ff3919ddabb6efeeab
NguyenHaThaiSon <mathsislife314@gmail.com>
2024-06-12 11:52:43 +0700

    Fixed interactive elements lagging behind when scrolling

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('click', function(event) {
     // Check if the click is on the body or main content area
-    if (event.target === event.currentTarget || event.target.closest('main')) {
+    if (event.target === event.currentTarget  || event.target.closest('main')) {
         const images = ['leaf1.svg', 'leaf2.svg', 'leaf3.svg', 'leaf4.svg', 'leaf5.svg']; 
         const randomIndex = Math.floor(Math.random() * images.length);
         const randomImage = images[randomIndex]; 
@@ -8,8 +8,8 @@ document.addEventListener('click', function(event) {
         leaf.src = 'leafs/' + randomImage; 
         leaf.className = 'leaf';
         // Adjust the position to account for the scroll position
-        leaf.style.top = `${event.clientY - 25}px`;
-        leaf.style.left = `${event.clientX - 25}px`;
+        leaf.style.top = `${event.clientY + window.scrollY - 25}px`;
+        leaf.style.left = `${event.clientX + window.scrollX - 25}px`;
         document.body.appendChild(leaf);
 
         // Add fade-out effect after a delay
